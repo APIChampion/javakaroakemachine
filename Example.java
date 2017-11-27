341e60fd1034f4be369629784d9c540750ceb333
brodb <brodb@allstate.com>
2017-11-27 14:52:00 -0600

    updated some stuff

--- Example.java
+++ Example.java
@@ -1,9 +1,16 @@
+import java.util.Date;
+
 import com.teamtreehouse.Treet;
+
 
 public class Example{
 
   public static void main(String[] args) {
-    Treet treet = new Treet();
+    Treet treet = new Treet(
+      "apichampion",
+      "ben is one of the best",
+            new Date(1511814180)
+      );
     System.out.printf("This is a new Treet: %s %n", treet);
   }
 }
